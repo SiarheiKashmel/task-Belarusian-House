08894b3a62a14130c4b3f298849430a95c374a57
Сергей Кашмель <107143266+SiarheiKashmel@users.noreply.github.com>
2022-07-26 18:29:29 +0400

    Добавьте файлы проекта.

---
+++ task Belarusian House/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Belarusian House");
+
+string entrance = Console.ReadLine();
+for (int i = 0; i < 10; i++)
+{
+
+    ConsoleColor[] cl = new ConsoleColor[] { ConsoleColor.White, ConsoleColor.Red, ConsoleColor.White };
+    for (int k = 0; k < 3; k++)
+    {
+        Console.BackgroundColor = cl[k];
+        Console.Write(entrance);
+    }
+    Console.WriteLine();
+}
+Console.BackgroundColor = ConsoleColor.Black;
